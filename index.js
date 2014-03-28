1993859d004c26b2b892208272c6904dd4350dbb
Brandon Farmer and Jonathan Jackson <dev+bthesorceror+rondale-sc@hashrocket.com>
2014-03-27 22:13:55 -0400

    Publish answers to survey_channel on redis room update

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-var io = require('socket.io').listen(8080);
+var _      = require('underscore');
+var io     = require('socket.io').listen(8080);
+var redis  = require('redis');
+var config = require('./config');
+
+var client = redis.createClient(config.redis.port, config.redis.host);
+
+var publish = _.throttle(function(survey_id, answers) {
+  io.sockets.in(survey_id).emit('answers', answers);
+}, 100);
+
+client.on('ready', function() {
+  client.subscribe('survey_channel');
+
+  client.on('message', function(channel, message) {
+    var object = JSON.parse(message);
+    publish(object.survey_id, object.answers);
+  });
+});
 
 io.sockets.on('connection', function (socket) {
+  socket.on('subscribe', function(survey) {
+    socket.join(survey);
+  });
 });
