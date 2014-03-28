93e64aaccff1a6e6f8e4148925e2f3c7176fc5f0
Jonathan Jackson <jonathan.jackson1@me.com>
2014-03-28 11:36:47 -0400

    Get auth properly

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var redis  = require('redis');
 if(process.env.REDISTOGO_URL) {
   var rtg    = require('url').parse(process.env.REDISTOGO_URL)
   var client = redis.createClient(rtg.port, rtg.hostname);
-  client.auth(rtg.auth.split(":")[1]);
+  client.auth(rtg.auth);
 } else {
   var client = redis.createClient();
 }
